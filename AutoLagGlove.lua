76cf182c50e6aa558af297bc70f8e46d695b3209
CAVİD <yashar.damir1@gmail.com>
2026-02-11 20:26:33 +0400

    Update teleportation and Anchored logic in AutoLagGlove
    Fixed teleportation logic and adjusted Anchored property.

--- AutoLagGlove.lua
+++ AutoLagGlove.lua
@@ -3,10 +3,10 @@ local c=p.Character or p.CharacterAdded:Wait()
 local h=c.HumanoidRootPart
 if not c:FindFirstChild("entered") then 
 h.CFrame=workspace.Lobby.Teleport1.CFrame 
-task.wait(1) 
+task.wait(1) sxddye
 end
 h.CFrame=CFrame.new(7.2714,-29.3799,275.2069,-0.1607,0,0.987,0,1,0,-0.987,0,-0.1607)
 task.wait(.5)
-h.Anchored=true
+h.Anchored=truex
 task.wait(10)
 game:GetService("Players").LocalPlayer:WaitForChild("Reset"):FireServer()
